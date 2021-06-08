d1b99a32607d594446c178a4494093d165117ec3
novakvova <novakvova@gmail.com>
2021-06-08 19:07:20 +0300

    index

---
+++ app/Models/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected  $table = 'events';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'created_at'
+    ];
+}
---
+++ database/migrations/2021_06_08_152101_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false);
+            $table->string('description', 4000)->nullable();
+            $table->string('image', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
